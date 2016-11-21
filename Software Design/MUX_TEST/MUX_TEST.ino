9ef6e781d45e3473a345572cce509a61db34caec
Jordan-Earl <williamsj15@wit.edu>
2016-11-21 11:18:25 -0500

    Header information for MUX_TEST

--- Software Design/MUX_TEST/MUX_TEST.ino
+++ Software Design/MUX_TEST/MUX_TEST.ino
@@ -1,4 +1,13 @@
-
+/* Copyright (c) Jordan Earl Williams
+ * Date Started: 19NOV2016
+ * Version 0.0.1
+ * 
+ * This code is to test the DEMUX theory of our LED matrix. What it does is runs the alarmOn fucntion 
+ * which makes the device go into the alarm state. With that we can set each LED to a color using PWM
+ * and DEMUX bit selection. Getting the RGB colors allows us to send it as a pwm to each cathode 
+ * allowing maximum color spectrum for our clock.
+ */
+ 
 int LED_R = 5, LED_B = 6, LED_G = 9, SEL_B0 = 4, SEL_B1 = 7, SEL_B2 = 8; //Setting pin numbers for the RGB Cathodes and the selection pins for the DEMUX
 int ACTIVE_RGB[18] = {200,25,0,200,25,0,200,25,0,200,25,0,200,25,0,200,25,0}; //This is to hold the colors for each LED when the alarm is on
 int DEACTIVE_RGB[18] = {0,75,220,0,75,220,0,75,220,0,75,220,0,75,220,0,75,220}; //This is to hold the colors for each LED when the correct button has been depressed
