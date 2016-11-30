311cc038cf672520e96aade91881f157ec11110f
G-JE <gunnarjerickson@gmail.com>
2016-11-30 16:28:27 -0500

    Gradle for Bluetooth Android App

---
+++ Software Design/Android_App/app/src/main/java/snuze/alarmtooth/MainActivity.java
@@ -0,0 +1,51 @@
+package snuze.alarmtooth;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.bluetooth.BluetoothAdapter;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BluetoothAdapter Adapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (Adapter == null){
+            System.out.print("Device does not support BlueTooth!");
+        }
+
+        if (!Adapter.isEnabled()){
+            Intent enableBT = new Intent(Adapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBT, REQUEST_ENABLE_BT);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
